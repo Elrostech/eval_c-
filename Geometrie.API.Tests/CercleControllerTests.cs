ae9768746cac2e431477447e228e419fab6e51f6
Elrostech <tlesergent@gmail.com>
2024-12-18 14:49:29 +0100

    avant test 2

---
+++ Geometrie.API.Tests/CercleControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Geometrie.API.Controllers;
+using Geometrie.BLL;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Geometrie.API.Tests
+{
+    public class CercleControllerTests
+    {
+        [Fact]
+        public void GetAll_ReturnsEmptyListInitially()
+        {
+            var controller = new CercleController();
+
+            var result = controller.GetAll();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var list = Assert.IsType<List<Cercle>>(okResult.Value);
+            Assert.Empty(list);
+        }
+
+        [Fact]
+        public void Add_AddsCercleSuccessfully()
+        {
+            var controller = new CercleController();
+            var cercle = new Cercle(new Point(0, 0), 5);
+
+            var result = controller.Add(cercle);
+
+            var createdResult = Assert.IsType<CreatedAtActionResult>(result);
+            Assert.Equal(cercle, createdResult.Value);
+        }
+
+        [Fact]
+        public void GetTotalArea_CalculatesCorrectly()
+        {
+            var controller = new CercleController();
+            var cercle1 = new Cercle(new Point(0, 0), 3); 
+            var cercle2 = new Cercle(new Point(1, 1), 4); 
+
+            controller.Add(cercle1);
+            controller.Add(cercle2);
+
+            var ids = new List<int> { 0, 1 };
+            var result = controller.GetTotalArea(ids);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var totalArea = Assert.IsType<double>(okResult.Value);
+            Assert.Equal(78.54, totalArea, 2);
+        }
+    }
+}
---
+++ Geometrie.API/Controllers/CercleController.cs
@@ -0,0 +1,62 @@
+﻿using Geometrie.BLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geometrie.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CercleController : Controller
+    {
+        private readonly List<Cercle> cercles = new();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(cercles);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var cercle = cercles.ElementAtOrDefault(id);
+            return cercle != null ? Ok(cercle) : NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Cercle cercle)
+        {
+            cercles.Add(cercle);
+            return CreatedAtAction(nameof(GetById), new { id = cercles.Count - 1 }, cercle);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Cercle cercle)
+        {
+            if (id < 0 || id >= cercles.Count)
+                return NotFound();
+
+            cercles[id] = cercle;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= cercles.Count)
+                return NotFound();
+
+            cercles.RemoveAt(id);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult GetTotalArea([FromBody] IEnumerable<int> ids)
+        {
+            double totalArea = ids
+                .Where(id => id >= 0 && id < cercles.Count)
+                .Sum(id => cercles[id].CalculerAire());
+
+            return Ok(totalArea);
+        }
+    }
+}
